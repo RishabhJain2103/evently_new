d0ba651f1708963fe6361b82ad5069ef36af2685
RishabhJain2103 <rishabhjainvio@gmail.com>
2024-07-12 16:47:08 +0530

    Create eventForm date2

--- components/shared/EventForm.tsx
+++ components/shared/EventForm.tsx
@@ -133,8 +133,8 @@ const EventForm = ( { userId , type}: EventFromProps) => {
                   <FormControl>
                     <div className="flex-center h-[52px] w-full overflow-hidden rounded-full bg-gray-50 px-4 py-2">
                         <Image 
-                         src='/assets/icons/calender.svg'
-                         alt="calender"
+                         src='/assets/icons/calendar.svg'
+                         alt="calendar"
                          width={24}
                          height={24}
                          className="filter-grey"
@@ -144,6 +144,44 @@ const EventForm = ( { userId , type}: EventFromProps) => {
                         <DatePicker 
                             selected={field.value} 
                             onChange={(date ) => field.onChange(date)} 
+                            showTimeSelect
+                            timeInputLabel="Time:"
+                            dateFormat={"dd/MM/yyyy h:mm:aa"}
+                            wrapperClassName="datePicker"
+                        />
+
+                    </div>
+
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+        </div>
+        <div className="flex flex-col gap-5 md:flex-row">
+            <FormField
+              control={form.control}
+              name="endDateTime"
+              render={({ field }) => (
+                <FormItem className="w-full">
+                  <FormControl>
+                    <div className="flex-center h-[52px] w-full overflow-hidden rounded-full bg-gray-50 px-4 py-2">
+                        <Image 
+                         src='/assets/icons/calendar.svg'
+                         alt="calendar"
+                         width={24}
+                         height={24}
+                         className="filter-grey"
+                        />
+                        <p className="ml-3 whitespace-nowrap text-green-600">End Date:</p>
+
+                        <DatePicker 
+                            selected={field.value} 
+                            onChange={(date ) => field.onChange(date)} 
+                            showTimeSelect
+                            timeInputLabel="Time:"
+                            dateFormat={"dd/MM/yyyy h:mm:aa"}
+                            wrapperClassName="datePicker"
                         />
 
                     </div>
