75e7bdcc98e98689ef1477408f6d1e337f9d6997
RishabhJain2103 <rishabhjainvio@gmail.com>
2024-07-14 15:37:52 +0530

    Create event to profile reload 1

--- lib/actions/event.actions.ts
+++ lib/actions/event.actions.ts
@@ -5,6 +5,13 @@ import { handleError } from "../utils"
 import { connectToDatabase } from "../mongodb/database"
 import User from "../mongodb/database/models/user.model"
 import Event from "../mongodb/database/models/event.model"
+import Category from "../mongodb/database/models/category.model"
+
+const populateEvent=async (query:any)=>{
+    return query
+     .populate({path:'organiser', model:User, select:`_id firstName lastName`})
+     .populate({path:'category', model:Category ,select:`_id name`})
+}
 
 export const createEvent= async ({event,userId,path}:CreateEventParams)=>{
     try {
@@ -21,5 +28,20 @@ export const createEvent= async ({event,userId,path}:CreateEventParams)=>{
         return JSON.parse(JSON.stringify(newEvent));
     } catch (error) {
         handleError(error);
+    }
+}
+export const getEventById=async (eventId:string)=>{
+    try {
+        await connectToDatabase();
+
+        const event=await populateEvent(Event.findById(eventId));
+
+        if (!event){
+            throw new Error("Event not Found")
+        }
+
+        return JSON.parse(JSON.stringify(event));
+    } catch (error) {
+        handleError(error)
     }
 }
